3ad5ccb6979a1a1862e70ecf36d9772292cbf1b5
RosalynCrothers <98907596+RosalynCrothers@users.noreply.github.com>
2023-09-12 13:03:01 +0100

    did stuff

--- labs/lab01/hello_world.cpp
+++ labs/lab01/hello_world.cpp
@@ -1,22 +1,56 @@
 #include <thread>
+#include <chrono>
+#include <random>
+#include <vector>
 #include <iostream>
 
-using namespace std ;
+using namespace std;
+using namespace std::chrono;
+using namespace std::this_thread;
+
+constexpr size_t num_threads = 100;
 
 /*
 This is the function called by the thread
 */
 void hello_world()
 {
-    cout << "Hello from thread " << this_thread::get_id() << endl ;
+    cout << "Hello from thread " << get_id() << endl ;
+}
+
+void task(size_t n, int val)
+{
+    //string s = "Thread: " + n + " Random Value: " + val;
+    cout << "Thread: " << n << " Random Value: " << val << endl;
 }
 
 int main(int argc, char **argv)
 {
+    /*
     // Create a new thread
     thread t(hello_world);
     // Wait for thread to finish (join it)
     t.join();
+    */
+
+    // C++ style of creating a random
+    // Seed with real random number if available
+    std::random_device r;
+    // Create random number generator
+    default_random_engine e(r());
+
+    //create a vector of threads
+    vector<thread> threads;
+
+    //go through a loop of 100, giving the threads an index and random number
+    for (size_t i = 0; i < num_threads; ++i)
+        threads.push_back(thread(task, i, e()));
+
+    // Use C++ ranged for loop to join the threads
+    // Same as foreach in C#
+    for (auto& t : threads)
+        t.join();
+
     // Return 0 (OK)
     return 0;
 }
--- labs/lab01/lambda-thread.cpp
+++ labs/lab01/lambda-thread.cpp
@@ -1,7 +1,19 @@
 #include <thread>
+#include <chrono>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+using namespace std::chrono;
+
+void work(int& n)
+{
+    n = 0;
+    for (size_t i = 0; i < 1000000; i++)
+    {
+        ++n;
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -9,6 +21,28 @@ int main(int argc, char **argv)
     thread t([]{ cout << "Hello from lambda thread!" << endl; });
     // Join thread
     t.join();
+
+    ofstream data("data1.csv", ofstream::out);
+    uint64_t total_ns = 0;
+    // We’re going to gather 100 readings , so create a thread and join it 100 times
+    for (int i = 0; i < 100; ++i)
+    {
+        // Get start time
+        auto start = system_clock::now();
+        // Start thread. Pass n as a reference, so that we can write to it from the thread
+        int n = 0;
+        thread t(work, ref(n));
+        t.join();
+        // Get end time
+        auto end = system_clock::now();
+        // Calculate the duration
+        auto total = end - start;
+        // Write to file
+        total_ns += total.count();
+        data << total.count() << endl;
+    }
+    data.close();
+    cout << "mean: " << total_ns / 100 << endl;
 
     return 0;
 }
